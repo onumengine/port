176140977b008a1f877749a21eb1a128fe28dad2
onumengine <onumdev@gmail.com>
2021-05-06 17:06:43 +0100

    create the appointment details screen skeleton

---
+++ lib/screens/appointment_details.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatefulWidget {
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
