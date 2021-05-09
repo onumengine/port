5cdd5d6b2f86858dca5f91bfee1e976d2a5dd714
onumengine <onumdev@gmail.com>
2021-05-09 17:08:43 +0100

    Finish the categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:port/components/atoms/searchbar.dart';
-import 'package:port/components/molecules/category_card.dart';
 import 'package:port/components/organisms/categories_grid.dart';
 
 class CategoriesScreen extends StatefulWidget {
