c5182fb39725ebef8d11981d1f031fe45d62379a
onumengine <onumdev@gmail.com>
2021-05-09 10:42:10 +0100

    Add horizontal padding to the categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -7,8 +7,18 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+  double _getHorizontalPadding(double screenWidth) {
+    if (screenWidth < 592)
+      return 20;
+    else if (screenWidth > 592 && screenWidth < 1000)
+      return 40;
+    else if (screenWidth > 1000) return 3;
+  }
+
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -23,6 +33,13 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
           style: TextStyle(
             fontWeight: FontWeight.w600,
           ),
+        ),
+      ),
+      body: Container(
+        height: screenSize.height,
+        width: screenSize.width,
+        padding: EdgeInsets.symmetric(
+          horizontal: _getHorizontalPadding(screenSize.width),
         ),
       ),
     );
