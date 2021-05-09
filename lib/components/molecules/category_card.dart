2ce2f336b0d24b3ade7409ad4342520b1b53ba18
onumengine <onumdev@gmail.com>
2021-05-09 12:17:56 +0100

    Nest a column in the category card

--- lib/components/molecules/category_card.dart
+++ lib/components/molecules/category_card.dart
@@ -18,6 +18,17 @@ class _CategoryCardState extends State<CategoryCard> {
       child: Container(
         height: 185,
         width: (MediaQuery.of(context).size.width / 3) * 2,
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 61,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+            ),
+            Text("Banks"),
+          ],
+        ),
       ),
     );
   }
