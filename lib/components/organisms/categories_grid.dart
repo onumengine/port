540cf106663ea65778755841317d14859ebeee52
onumengine <onumdev@gmail.com>
2021-05-09 11:04:48 +0100

    Start building the category grid

---
+++ lib/components/organisms/categories_grid.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+class CategoriesGrid extends StatefulWidget {
+  @override
+  _CategoriesGridState createState() => _CategoriesGridState();
+}
+
+class _CategoriesGridState extends State<CategoriesGrid> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
