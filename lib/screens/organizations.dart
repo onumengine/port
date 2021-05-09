ea554a5d5c23fda247999f69ce4ccb7b8ab2ff34
onumengine <onumdev@gmail.com>
2021-05-09 21:35:43 +0100

    Start the organizations screen

---
+++ lib/screens/organizations.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+class OrganizationsScreen extends StatefulWidget {
+  @override
+  _OrganizationsScreenState createState() => _OrganizationsScreenState();
+}
+
+class _OrganizationsScreenState extends State<OrganizationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
