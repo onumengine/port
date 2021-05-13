1c8f2e2697d46671d1ae5158267151847dda14c8
onumengine <onumdev@gmail.com>
2021-05-13 20:15:55 +0100

    Create a tab bar for the months

---
+++ lib/components/organisms/calendar.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+
+class Calendar extends StatefulWidget {
+  TabController controller;
+
+  Calendar({
+    @required this.controller,
+  });
+
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      controller: widget.controller,
+      tabs: <Widget>[
+        Text("January"),
+        Text("February"),
+        Text("March"),
+        Text("April"),
+        Text("May"),
+        Text("June"),
+      ],
+    );
+  }
+}
