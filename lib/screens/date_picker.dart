cf6b86c2f6336f611aa5a195a2c677f928f099f6
onumengine <onumdev@gmail.com>
2021-05-13 16:21:40 +0100

    Start the date picker screen

---
+++ lib/screens/date_picker.dart
@@ -0,0 +1,23 @@
+import "package:flutter/cupertino.dart";
+import "package:flutter/material.dart";
+
+class DatePickerScreen extends StatefulWidget {
+  @override
+  _DatePickerScreenState createState() => _DatePickerScreenState();
+}
+
+class _DatePickerScreenState extends State<DatePickerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
