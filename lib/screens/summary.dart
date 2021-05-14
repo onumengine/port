252fe3e11d1aa40e1f3396ae54942cced7b3d655
onumengine <onumdev@gmail.com>
2021-05-14 20:37:25 +0100

    Wrap the dropdown button in a grey container

--- lib/screens/summary.dart
+++ lib/screens/summary.dart
@@ -95,45 +95,53 @@ class _SubmitScreenState extends State<SubmitScreen> {
                 ),
                 child: Column(
                   children: [
-                    DropdownButton<AppointmentReasons>(
-                      isExpanded: true,
-                      value: _dropdownButtonValue,
-                      icon: Icon(CupertinoIcons.chevron_down),
-                      underline: SizedBox(),
-                      onChanged: (selectedAppointmentReason) {
-                        setState(() {
-                          _dropdownButtonValue = selectedAppointmentReason;
-                        });
-                        print("${_dropdownButtonValue}");
-                      },
-                      hint: Text("Choose reason for appointment"),
-                      disabledHint: Text("Disabled"),
-                      items: <DropdownMenuItem<AppointmentReasons>>[
-                        DropdownMenuItem(
-                          child: Container(
-                            child: Center(
-                              child: Text("Reason1"),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                        color: inputBackgroundColor,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: DropdownButton<AppointmentReasons>(
+                        isExpanded: true,
+                        value: _dropdownButtonValue,
+                        icon: Icon(CupertinoIcons.chevron_down),
+                        iconEnabledColor: opPrimaryColor,
+                        underline: SizedBox(),
+                        onChanged: (selectedAppointmentReason) {
+                          setState(() {
+                            _dropdownButtonValue = selectedAppointmentReason;
+                          });
+                          print("${_dropdownButtonValue}");
+                        },
+                        hint: Text("Choose reason for appointment"),
+                        disabledHint: Text("Disabled"),
+                        items: <DropdownMenuItem<AppointmentReasons>>[
+                          DropdownMenuItem(
+                            child: Container(
+                              child: Center(
+                                child: Text("Reason1"),
+                              ),
                             ),
+                            value: AppointmentReasons.reason1,
                           ),
-                          value: AppointmentReasons.reason1,
-                        ),
-                        DropdownMenuItem(
-                          child: Container(
-                            child: Center(
-                              child: Text("Reason2"),
+                          DropdownMenuItem(
+                            child: Container(
+                              child: Center(
+                                child: Text("Reason2"),
+                              ),
                             ),
+                            value: AppointmentReasons.reason2,
                           ),
-                          value: AppointmentReasons.reason2,
-                        ),
-                        DropdownMenuItem(
-                          child: Container(
-                            child: Center(
-                              child: Text("Reason3"),
+                          DropdownMenuItem(
+                            child: Container(
+                              child: Center(
+                                child: Text("Reason3"),
+                              ),
                             ),
+                            value: AppointmentReasons.reason3,
                           ),
-                          value: AppointmentReasons.reason3,
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                     SizedBox(height: 27),
                     Align(
