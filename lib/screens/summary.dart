e3befb85443be59984139e2d18620b38fb0efbe3
onumengine <onumdev@gmail.com>
2021-05-14 06:38:12 +0100

    Integrate the reusable  timestamp card in the summary screen

--- lib/screens/summary.dart
+++ lib/screens/summary.dart
@@ -1,6 +1,7 @@
 import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import 'package:port/components/atoms/appointment_status_tile.dart';
+import 'package:port/components/organisms/timestamp_card.dart';
 import 'package:port/utility/colors.dart';
 import 'package:port/utility/colors_main.dart';
 
@@ -65,109 +66,7 @@ class _SubmitScreenState extends State<SubmitScreen> {
             ),
           ),
           SizedBox(height: 20),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: _getHorizontalPadding(screenSize.width),
-            ),
-            child: PhysicalModel(
-              color: colorAppBackground,
-              shadowColor: colorCardShadow,
-              elevation: 16,
-              borderRadius: BorderRadius.circular(8),
-              child: Container(
-                height: 228,
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Day and Time",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: primaryTextColor,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Container(
-                      height: 41,
-                      decoration: BoxDecoration(
-                        color: inputBackgroundColor,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 8.0, right: 12),
-                            child: Icon(
-                              Icons.watch_later,
-                              size: 15,
-                            ),
-                          ),
-                          Text(
-                            "Monday, 20 July 2021",
-                            style: TextStyle(fontWeight: FontWeight.w400),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Container(
-                      height: 41,
-                      decoration: BoxDecoration(
-                        color: inputBackgroundColor,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 8.0, right: 12),
-                            child: Icon(
-                              Icons.watch_later,
-                              size: 15,
-                            ),
-                          ),
-                          Text(
-                            "9:00 AM",
-                            style: TextStyle(fontWeight: FontWeight.w400),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Container(
-                      height: 41,
-                      decoration: BoxDecoration(
-                        color: inputBackgroundColor,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 8.0, right: 12),
-                            child: Icon(
-                              Icons.watch_later,
-                              size: 15,
-                            ),
-                          ),
-                          Text(
-                            "30 minutes",
-                            style: TextStyle(fontWeight: FontWeight.w400),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          TimestampCard(),
           SizedBox(height: 20),
           Padding(
             padding: EdgeInsets.symmetric(
