714fcfa922f220d3817bafad3fc71151da05daf5
onumengine <onumdev@gmail.com>
2021-05-16 01:18:50 +0100

    Creae the calendar button row

--- lib/components/organisms/calendar.dart
+++ lib/components/organisms/calendar.dart
@@ -6,8 +6,55 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
+  int year = 2021;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      height: 540,
+      color: Colors.blueAccent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: _decrementYear,
+              ),
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: _decrementYear,
+              ),
+              Text("${this.year}"),
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: _incrementYear,
+              ),
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: _incrementYear,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _incrementYear() {
+    setState(() {
+      year++;
+    });
+  }
+
+  void _decrementYear() {
+    setState(() {
+      year--;
+    });
   }
 }
