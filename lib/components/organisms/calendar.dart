3756865257ea3295cdbfaa5edb03948d1ce29184
onumengine <onumdev@gmail.com>
2021-05-16 17:54:24 +0100

    Write methods for incrementing and decrementing the month

--- lib/components/organisms/calendar.dart
+++ lib/components/organisms/calendar.dart
@@ -2,6 +2,7 @@ import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import 'package:port/components/atoms/calendar_bubble.dart';
 import 'package:port/utility/colors.dart';
+import 'package:port/utility/colors_main.dart';
 
 class Calendar extends StatefulWidget {
   @override
@@ -13,6 +14,21 @@ class _CalendarState extends State<Calendar> {
   DateTime _dateTime;
   int _currentMonth, _currentDay, _currentYear;
 
+  Map<int, String> _numberToMonthMap = <int, String>{
+    1: "Jan",
+    2: "Feb",
+    3: "Mar",
+    4: "Apr",
+    5: "May",
+    6: "Jun",
+    7: "Jul",
+    8: "Aug",
+    9: "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec",
+  };
+
   @override
   void initState() {
     super.initState();
@@ -21,85 +37,89 @@ class _CalendarState extends State<Calendar> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 540,
-      color: Colors.blueAccent,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                icon: Icon(CupertinoIcons.chevron_left_2),
-                onPressed: _decrementYear,
-              ),
-              IconButton(
-                icon: Icon(CupertinoIcons.chevron_left),
-                onPressed: _decrementYear,
-              ),
-              Column(
-                children: <Widget>[
-                  Text(
-                    "$_year",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 20,
-                    ),
-                  ),
-                  Text("Month"),
-                ],
-              ),
-              IconButton(
-                icon: Icon(CupertinoIcons.chevron_right),
-                onPressed: _incrementYear,
-              ),
-              IconButton(
-                icon: Icon(CupertinoIcons.chevron_right_2),
-                onPressed: _incrementYear,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Text("M"),
-              Text("T"),
-              Text("W"),
-              Text("T"),
-              Text("F"),
-              Text("S"),
-              Text("S"),
-            ],
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.purpleAccent,
-              padding: EdgeInsets.all(8),
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 7,
-                  mainAxisSpacing: 8,
-                  crossAxisSpacing: 8,
+    return PhysicalModel(
+      color: colorAppBackground,
+      shadowColor: colorCardShadow,
+      elevation: 8,
+      borderRadius: BorderRadius.circular(8),
+      child: Container(
+        width: double.infinity,
+        height: 540,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Icon(CupertinoIcons.chevron_left_2),
+                  onPressed: _decrementYear,
                 ),
-                itemBuilder: (context, index) {
-                  return CalendarBubble(
-                    text: (index + 1).toString(),
-                    onTap: () {
-                      print("Tapped the calendar bubble ${index + 1}");
-                      print(_dateTime);
-                    },
-                  );
-                },
-                itemCount: 28,
+                IconButton(
+                  icon: Icon(CupertinoIcons.chevron_left),
+                  onPressed: _decrementYear,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "$_year",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text("Month"),
+                  ],
+                ),
+                IconButton(
+                  icon: Icon(CupertinoIcons.chevron_right),
+                  onPressed: _incrementYear,
+                ),
+                IconButton(
+                  icon: Icon(CupertinoIcons.chevron_right_2),
+                  onPressed: _incrementYear,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text("M"),
+                Text("T"),
+                Text("W"),
+                Text("T"),
+                Text("F"),
+                Text("S"),
+                Text("S"),
+              ],
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(8),
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 7,
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
+                  ),
+                  itemBuilder: (context, index) {
+                    return CalendarBubble(
+                      text: (index + 1).toString(),
+                      onTap: () {
+                        print("Tapped the calendar bubble ${index + 1}");
+                        print(_dateTime);
+                      },
+                    );
+                  },
+                  itemCount: 28,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -116,7 +136,23 @@ class _CalendarState extends State<Calendar> {
     });
   }
 
-  void _incrementMonth() {}
+  void _incrementMonth() {
+    setState(() {
+      if (_currentMonth == 12) {
+        _currentMonth = 0;
+      } else {
+        _currentMonth++;
+      }
+    });
+  }
 
-  void _decrementMonth() {}
+  void _decrementMonth() {
+    setState(() {
+      if (_currentMonth == 1) {
+        _currentMonth = 12;
+      } else {
+        _currentMonth--;
+      }
+    });
+  }
 }
