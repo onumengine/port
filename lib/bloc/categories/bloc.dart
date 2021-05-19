7ed50a01414876ba8a0a0d9696d56db9aa934012
onumengine <onumdev@gmail.com>
2021-05-19 13:30:41 +0100

    create the categories bloc

---
+++ lib/bloc/categories/bloc.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CategoriesBloc extends Bloc {
+  @override
+  // TODO: implement initialState
+  get initialState => throw UnimplementedError();
+
+  @override
+  Stream mapEventToState(event) {
+    // TODO: implement mapEventToState
+    throw UnimplementedError();
+  }
+}
