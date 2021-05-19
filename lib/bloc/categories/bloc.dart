56131ac1b42db755eae62a2524c97541ed3a911e
onumengine <onumdev@gmail.com>
2021-05-19 15:30:44 +0100

    Initialize the ApiClient in the categories BLoC

--- lib/bloc/categories/bloc.dart
+++ lib/bloc/categories/bloc.dart
@@ -1,12 +1,17 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:port/bloc/categories/event.dart';
+import 'package:port/bloc/categories/state.dart';
+import 'package:port/repository/api_client.dart';
 
-class CategoriesBloc extends Bloc {
+class CategoriesBloc extends Bloc<CategoriesEvent, CategoriesState> {
+  ApiClient _apiClient = ApiClient();
+
   @override
   // TODO: implement initialState
   get initialState => throw UnimplementedError();
 
   @override
-  Stream mapEventToState(event) {
+  Stream<CategoriesState> mapEventToState(CategoriesEvent event) async* {
     // TODO: implement mapEventToState
     throw UnimplementedError();
   }
--- lib/bloc/categories/event.dart
+++ lib/bloc/categories/event.dart
@@ -4,3 +4,9 @@ abstract class CategoriesEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class CategoriesFetchingError extends CategoriesEvent {}
+
+class CategoriesFetchedEvent extends CategoriesEvent {}
+
+class CategoriesPopulatedSuccessEvent extends CategoriesEvent {}
