3968cc55be9522775b3f89a720e401fb90b72508
onumengine <onumdev@gmail.com>
2021-05-20 01:52:42 +0100

    implement the mapEventToState  method override

--- lib/bloc/notifications/bloc.dart
+++ lib/bloc/notifications/bloc.dart
@@ -5,4 +5,15 @@ import 'package:port/bloc/notifications/state.dart';
 class NotificationBloc extends Bloc<NotificationEvent, NotificationState> {
   @override
   NotificationState get initialState => EmptyNotificationState();
+
+  @override
+  Stream<NotificationState> mapEventToState(NotificationEvent event) async* {
+    if (event is NotificationRefreshEvent) {
+      yield NotificationRefreshingState();
+    } else if (event is NotificationRefreshSuccessEvent) {
+      yield NonEmptyNotificationState();
+    } else if (event is NotificationRefreshErrorEvent) {
+      yield NotificationFetchUnsuccessfulState();
+    }
+  }
 }
