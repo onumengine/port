396e1832192b793a0c6f187877a15bd267ad7bfa
onumengine <onumdev@gmail.com>
2021-05-21 00:51:59 +0100

    map the organization bloc's events to its states

---
+++ lib/bloc/organizations/bloc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:port/bloc/organizations/event.dart';
+import 'package:port/bloc/organizations/state.dart';
+
+class OrganizationsBloc extends Bloc<OrganizationsEvent, OrganizationsState> {
+  @override
+  OrganizationsState get initialState => EmptyOrganizationsState();
+
+  @override
+  Stream<OrganizationsState> mapEventToState(OrganizationsEvent event) async* {
+    if (event is OrganizationsFetchingEvent) {
+      yield OrganizationsFetchingState();
+    } else if (event is OrganizationsFetchingSuccessEvent) {
+      yield NonEmptyOrganizationsState();
+    } else if (event is OrganizationsFetchingErrorEvent) {
+      yield OrganizationsFetchingErrorState();
+    }
+  }
+}
