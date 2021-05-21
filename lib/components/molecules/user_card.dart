89c6b048863136ea59237d73e638b3fab2d18f34
onumengine <onumdev@gmail.com>
2021-05-21 11:53:26 +0100

    make the user card get its data through its constructor

--- lib/components/molecules/user_card.dart
+++ lib/components/molecules/user_card.dart
@@ -4,25 +4,29 @@ import 'package:port/screens/scheduler.dart';
 import 'package:port/utility/colors.dart';
 
 class UserCard extends StatelessWidget {
+  String imagePath, semanticLabel, name, jobTitle;
+
+  UserCard({this.imagePath, this.semanticLabel, this.name, this.jobTitle});
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: ListTile(
         leading: SvgPicture.asset(
-          "images/mike.svg",
+          this.imagePath,
           height: 64,
           width: 64,
-          semanticsLabel: "Mike image",
+          semanticsLabel: this.semanticLabel,
         ),
         title: Text(
-          "Mike456",
+          this.name,
           style: TextStyle(
             fontWeight: FontWeight.w500,
             fontSize: 18,
           ),
         ),
         subtitle: Text(
-          "Receptionist",
+          this.jobTitle,
           style: TextStyle(
             fontWeight: FontWeight.w400,
             fontSize: 16,
