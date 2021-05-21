88fa3bdf79b69861b6c1bc685019dde1f068e557
onumengine <onumdev@gmail.com>
2021-05-21 10:49:06 +0100

    Create the users states

---
+++ lib/bloc/users/state.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+abstract class UsersScreenState extends Equatable {}
+
+class UnfetchedUsersState extends UsersScreenState {
+  @override
+  List<Object> get props => [];
+}
+
+class PopulatedUsersState extends UsersScreenState {
+  @override
+  List<Object> get props => [];
+}
+
+class FetchingErrorState extends UsersScreenState {
+  @override
+  List<Object> get props => [];
+}
