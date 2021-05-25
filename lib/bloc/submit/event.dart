144b091a14e6c8dc5b179d011243c77a7a943f5e
onumdev <onumdev@gmail.com>
2021-05-25 13:13:21 +0100

    Create submit screen event classes

---
+++ lib/bloc/submit/event.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+abstract class SubmitScreenEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class SubmissionEvent extends SubmitScreenEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class SubmissionSuccessEvent extends SubmitScreenEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class SubmissionError extends SubmitScreenEvent {
+  @override
+  List<Object> get props => [];
+}
