4d4dddf6801e010637b3b2d975065f1af17fb03f
onumdev <onumdev@gmail.com>
2021-05-25 03:44:43 +0100

    Create a new initialization SchedulerEvent

--- lib/bloc/scheduler/event.dart
+++ lib/bloc/scheduler/event.dart
@@ -2,6 +2,11 @@ import 'package:equatable/equatable.dart';
 
 abstract class SchedulerEvent extends Equatable {}
 
+class DatePickerInitializationEvent extends SchedulerEvent {
+  @override
+  List<Object> get props => [];
+}
+
 /// When I increment a variable, the new value will be passed to the state constructor
 /// with [mapEventToState()]. The same goes for decrementing the variable.
 class MonthIncrementEvent extends SchedulerEvent {
