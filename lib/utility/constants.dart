a76d8915b39aee726aa7cd36c6b1f2c7bf04971f
onumdev <onumdev@gmail.com>
2021-05-31 16:39:51 +0100

    change the api endpoint

--- lib/utility/constants.dart
+++ lib/utility/constants.dart
@@ -2,8 +2,9 @@ library constants;
 
 import 'dart:convert';
 
-const String BASE_URL = "https://virtualmerenda.000webhostapp.com/vetlyn/index.php/";
-
+const String BASE_URLOLD =
+    "https://virtualmerenda.000webhostapp.com/vetlyn/index.php/";
+const String BASE_URL = "http://innovativehill.com/port/api/index.php/";
 
 // http client constants
 var headers = <String, String>{
@@ -25,5 +26,4 @@ const String LOGGED_IN_USER = "user";
 
 const String LOGIN_STRING_REQUEST = BASE_URL + "user/login";
 
-const String CREATE_STUDENT_PROFILE =
-    BASE_URL + "";
+const String CREATE_STUDENT_PROFILE = BASE_URL + "";
