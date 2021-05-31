efbb0586a43c11a28381a14d6b21cad23d130835
onumdev <onumdev@gmail.com>
2021-05-31 17:21:12 +0100

    delete an unnecessary state class

--- lib/bloc/categories/state.dart
+++ lib/bloc/categories/state.dart
@@ -1,14 +1,23 @@
 import 'package:equatable/equatable.dart';
 
 abstract class CategoriesState extends Equatable {
+  @override
+  List<Object> get props => [];
 }
 
 class CategoriesInitialState extends CategoriesState {
+}
+
+class CategoriesFetchingState extends CategoriesState {}
+
+class CategoriesFetchedState extends CategoriesState {
+  List<dynamic> categories;
+
+  CategoriesFetchedState({this.categories});
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [categories];
 }
 
 class ErrorState extends CategoriesState {
-  @override
-  List<Object> get props => [];
 }
