21fb3e73437a8f88300827a744604f7880b7b3f2
onumdev <onumdev@gmail.com>
2021-05-31 16:49:41 +0100

    create a CategoriesInitialState class

--- lib/bloc/categories/state.dart
+++ lib/bloc/categories/state.dart
@@ -1,8 +1,14 @@
 import 'package:equatable/equatable.dart';
 
 abstract class CategoriesState extends Equatable {
+}
+
+class CategoriesInitialState extends CategoriesState {
   @override
   List<Object> get props => [];
 }
 
-class ErrorState extends CategoriesState {}
+class ErrorState extends CategoriesState {
+  @override
+  List<Object> get props => [];
+}
