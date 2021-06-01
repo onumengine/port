38c9687f026732767a395a8f0dad289a7bcbee77
onumdev <onumdev@gmail.com>
2021-06-01 18:52:19 +0100

    Add state variables to the DatePickerState class

--- lib/bloc/date_picker/state.dart
+++ lib/bloc/date_picker/state.dart
@@ -1,6 +1,26 @@
 import 'package:equatable/equatable.dart';
 
 class DatePickerState extends Equatable {
+  int selectedYear,
+      selectedMonth,
+      selectedDay,
+      numberOfDaysInSelectedMonth,
+      numberOfCalendarSpacesToSkip;
+
+  DatePickerState({
+    selectedYear,
+    selectedMonth,
+    selectedDay,
+    numberOfDaysInSelectedMonth,
+    numberOfCalendarSpacesToSkip,
+  });
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [
+        selectedYear,
+        selectedMonth,
+        selectedDay,
+        numberOfDaysInSelectedMonth,
+        numberOfCalendarSpacesToSkip,
+      ];
 }
