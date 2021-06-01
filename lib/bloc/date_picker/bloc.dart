91d5ab0322cffcdeeb1dbfbb972f6feeb2435fb4
onumdev <onumdev@gmail.com>
2021-06-01 19:31:43 +0100

    Initialize the DatePickerBloc

--- lib/bloc/date_picker/bloc.dart
+++ lib/bloc/date_picker/bloc.dart
@@ -3,8 +3,21 @@ import 'package:port/bloc/date_picker/event.dart';
 import 'package:port/bloc/date_picker/state.dart';
 
 class DatePickerBloc extends Bloc<DatePickerEvent, DatePickerState> {
+
+  int selectedYear, selectedMonth, selectedDay;
+
+  DatePickerBloc() {
+    selectedYear = DateTime.now().year;
+    selectedMonth = DateTime.now().month;
+    selectedDay = DateTime.now().day;
+  }
+
   @override
-  DatePickerState get initialState => throw UnimplementedError();
+  DatePickerState get initialState => DatePickerState(
+    selectedYear: DateTime.now().year,
+    selectedMonth: DateTime.now().month,
+    selectedDay: DateTime.now().day,
+  );
 
   @override
   Stream<DatePickerState> mapEventToState(DatePickerEvent event) {
