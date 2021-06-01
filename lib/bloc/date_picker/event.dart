c095f001dc525c93de7aae6cf51fb0cd4f95febf
onumdev <onumdev@gmail.com>
2021-06-01 19:28:23 +0100

    Create different date picker event classes

---
+++ lib/bloc/date_picker/event.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+abstract class DatePickerEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class YearIncrementEvent extends DatePickerEvent {}
+
+class YearDecrementEvent extends DatePickerEvent {}
+
+class MonthIncrementEvent extends DatePickerEvent {}
+
+class MonthDecrementEvent extends DatePickerEvent {}
+
+class DaySelectionEvent extends DatePickerEvent {
+  int day;
+
+  DaySelectionEvent(this.day);
+
+  @override
+  List<Object> get props => [];
+}
