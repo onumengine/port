5e3b06a249f0d95bb1710df0a9c3c39ccc6987bd
onumdev <onumdev@gmail.com>
2021-06-01 19:35:32 +0100

    Delete event classes related to the DatePicker widget from the Scheduler event file

--- lib/bloc/scheduler/event.dart
+++ lib/bloc/scheduler/event.dart
@@ -2,42 +2,6 @@ import 'package:equatable/equatable.dart';
 
 abstract class SchedulerEvent extends Equatable {}
 
-class DatePickerInitializationEvent extends SchedulerEvent {
-  @override
-  List<Object> get props => [];
-}
-
-/// When I increment a variable, the new value will be passed to the state constructor
-/// with [mapEventToState()]. The same goes for decrementing the variable.
-class MonthIncrementEvent extends SchedulerEvent {
-  @override
-  List<Object> get props => [];
-}
-
-class MonthDecrementEvent extends SchedulerEvent {
-  @override
-  List<Object> get props => [];
-}
-
-class YearIncrementEvent extends SchedulerEvent {
-  @override
-  List<Object> get props => [];
-}
-
-class YearDecrementEvent extends SchedulerEvent {
-  @override
-  List<Object> get props => [];
-}
-
-class DaySelectionEvent extends SchedulerEvent {
-  int selectedDay;
-
-  DaySelectionEvent(this.selectedDay);
-
-  @override
-  List<Object> get props => [selectedDay];
-}
-
 class TimeSelectionEvent extends SchedulerEvent {
   @override
   List<Object> get props => [];
