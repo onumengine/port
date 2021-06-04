5a31fb4423ecf9e152ca9dd7aee9b4482ca9571c
onumdev <onumdev@gmail.com>
2021-06-04 23:33:15 +0100

    Add event variables to the UsersFetchEvent

--- lib/bloc/users/event.dart
+++ lib/bloc/users/event.dart
@@ -3,6 +3,10 @@ import 'package:equatable/equatable.dart';
 abstract class UsersScreenEvent extends Equatable {}
 
 class UsersFetchEvent extends UsersScreenEvent {
+  String usersOrganizationId;
+
+  UsersFetchEvent({this.usersOrganizationId});
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [this.usersOrganizationId];
 }
