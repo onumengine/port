a55fb782972295618864275d5515135b13907ec6
onumdev <onumdev@gmail.com>
2021-06-04 11:00:22 +0100

    Define a method for fetching organizations

--- lib/bloc/organizations/bloc.dart
+++ lib/bloc/organizations/bloc.dart
@@ -1,10 +1,17 @@
+import 'dart:convert';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:port/bloc/organizations/event.dart';
 import 'package:port/bloc/organizations/state.dart';
+import 'package:port/repository/api_client.dart';
+import 'package:port/utility/constants.dart';
 
 class OrganizationsBloc extends Bloc<OrganizationsEvent, OrganizationsState> {
+  ApiClient _apiClient = ApiClient();
+  var response;
+
   @override
-  OrganizationsState get initialState => NonEmptyOrganizationsState();
+  OrganizationsState get initialState => EmptyOrganizationsState();
 
   @override
   Stream<OrganizationsState> mapEventToState(OrganizationsEvent event) async* {
@@ -15,5 +22,10 @@ class OrganizationsBloc extends Bloc<OrganizationsEvent, OrganizationsState> {
     } else if (event is OrganizationsFetchingErrorEvent) {
       yield OrganizationsFetchingErrorState();
     }
+  }
+
+  Future<void> fetchOrganizations(String organizationId) async {
+    response = jsonDecode(await _apiClient.get(CATEGORIES_FETCH_PATH));
+    print(response);
   }
 }
