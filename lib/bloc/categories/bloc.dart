eaaf852496dc64e753c34388cfa1f7389319ee08
onumdev <onumdev@gmail.com>
2021-06-04 09:13:48 +0100

    make the CategoriesBloc get categories from the API when the CategoriesScreen initState method is called

--- lib/bloc/categories/bloc.dart
+++ lib/bloc/categories/bloc.dart
@@ -1,12 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:port/bloc/categories/event.dart';
 import 'package:port/bloc/categories/state.dart';
 import 'package:port/repository/api_client.dart';
-import 'package:port/repository/app_exceptions.dart';
 import 'package:port/utility/constants.dart';
 
 class CategoriesBloc extends Bloc<CategoriesEvent, CategoriesState> {
   ApiClient _apiClient = ApiClient();
+  Map<String, dynamic> response;
+  var errorMessage, listOfCategories;
 
   @override
   CategoriesState get initialState => CategoriesInitialState();
@@ -15,20 +18,18 @@ class CategoriesBloc extends Bloc<CategoriesEvent, CategoriesState> {
   Stream<CategoriesState> mapEventToState(CategoriesEvent event) async* {
     if (event is CategoriesFetchEvent) {
       yield CategoriesFetchingState();
-      var response, responseObject, categoriesList;
       try {
-        response = _apiClient.get(CATEGORIES_FETCH_PATH);
-        responseObject = Future.value(responseObject);
-        print("The response from fetching categories is $responseObject");
-        categoriesList = responseObject["data"];
-        print("The list of categories in the BLoC is $categoriesList");
-        yield CategoriesFetchedState(categories: response);
-      } on FetchDataException catch (e) {
-        print("Categories fetch caught an error. ERROR: $e");
-        yield ErrorState(errorMessage: e.toString());
-      } finally {
-        yield CategoriesFetchedState(categories: response["data"]);
+        await fetchCategories();
+        listOfCategories = response["data"];
+        yield CategoriesFetchedState(categories: listOfCategories);
+      } catch (e) {
+        errorMessage = e.toString();
+        yield ErrorState(errorMessage: errorMessage);
       }
     }
+  }
+
+  Future<void> fetchCategories() async {
+    response = jsonDecode(await _apiClient.get(CATEGORIES_FETCH_PATH));
   }
 }
