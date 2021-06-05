f483cb953a8fba550b8c3e397badc20cf5470ba0
onumdev <onumdev@gmail.com>
2021-06-05 14:11:10 +0100

    Yield PopulatedUsersState when the API fetch returns a result

--- lib/bloc/users/bloc.dart
+++ lib/bloc/users/bloc.dart
@@ -1,8 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:port/bloc/users/event.dart';
 import 'package:port/bloc/users/state.dart';
+import 'package:port/repository/api_client.dart';
+import 'package:port/utility/constants.dart';
 
 class UsersBloc extends Bloc<UsersScreenEvent, UsersScreenState> {
+  ApiClient _apiClient = ApiClient();
+  Map<String, dynamic> response;
+  List listOfUsers;
+
   @override
   UsersScreenState get initialState => PopulatedUsersState();
 
@@ -10,6 +18,19 @@ class UsersBloc extends Bloc<UsersScreenEvent, UsersScreenState> {
   Stream<UsersScreenState> mapEventToState(UsersScreenEvent event) async* {
     if (event is UsersFetchEvent) {
       yield FetchingUsersState();
+      try {
+        await _fetchUsers(event.usersOrganizationId);
+        listOfUsers = response["data"];
+        print("YOUR LIST OF USERS IS: $listOfUsers");
+        yield PopulatedUsersState(users: listOfUsers);
+      } catch (error) {
+        yield FetchingErrorState(errorMessage: error.toString());
+      }
     }
+  }
+
+  Future<void> _fetchUsers(String usersOrganizationId) async {
+    response = jsonDecode(await _apiClient.get(USERS_FETCH_PATH + usersOrganizationId));
+    print("YOUR RESPONSE IS: $response");
   }
 }
