a78beaf2133dd06e318aff019548daeb97cc480f
onumdev <onumdev@gmail.com>
2021-06-06 23:49:58 +0100

    Store the reusable TimeButton  selectedState in a boolean member

--- lib/components/atoms/time_button.dart
+++ lib/components/atoms/time_button.dart
@@ -2,10 +2,14 @@ import "package:flutter/material.dart";
 import 'package:port/utility/colors.dart';
 
 class TimeButton extends StatefulWidget {
+  bool isSelected;
+  String label;
+
   _TimeButtonState createState() => _TimeButtonState();
 }
 
 class _TimeButtonState extends State<TimeButton> {
+
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
@@ -14,16 +18,16 @@ class _TimeButtonState extends State<TimeButton> {
       height: 62,
       width: screenSize.width / 14,
       decoration: BoxDecoration(
-        color: paleChipBackground,
+        color: (widget.isSelected) ? opPrimaryColor : paleChipBackground,
         borderRadius: BorderRadius.circular(12),
       ),
       child: Center(
         child: Text(
-          "11:00",
+          widget.label,
           style: TextStyle(
             fontWeight: FontWeight.w600,
             fontSize: 18,
-            color: chipTextColorDisabled,
+            color: (widget.isSelected) ? opBackgroundColor : chipTextColorDisabled,
           ),
         ),
       ),
