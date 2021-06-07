41d7109fcf0a3d2b6557aad6bce5151ecc479c1e
onumdev <onumdev@gmail.com>
2021-06-07 07:55:09 +0100

    Add the selectedButtonIndex to the TimeSelectionEvent class

--- lib/bloc/scheduler/event.dart
+++ lib/bloc/scheduler/event.dart
@@ -3,8 +3,12 @@ import 'package:equatable/equatable.dart';
 abstract class SchedulerEvent extends Equatable {}
 
 class TimeSelectionEvent extends SchedulerEvent {
+  int selectedButtonIndex;
+
+  TimeSelectionEvent({this.selectedButtonIndex});
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [this.selectedButtonIndex];
 }
 
 class DurationSelectionEvent extends SchedulerEvent {
