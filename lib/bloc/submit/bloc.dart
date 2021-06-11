8098ac360193a15559c646b8ed5fefe5db81aebd
onumdev <onumdev@gmail.com>
2021-06-11 07:36:56 +0100

    handle different events in the SubmitBloc

--- lib/bloc/submit/bloc.dart
+++ lib/bloc/submit/bloc.dart
@@ -1,50 +1,64 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:port/bloc/submit/event.dart';
 import 'package:port/bloc/submit/state.dart';
 import 'package:port/repository/api_client.dart';
 import 'package:port/utility/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SubmitBloc extends Bloc<SubmitScreenEvent, SubmitScreenState> {
   ApiClient _apiClient = ApiClient();
   Map<String, dynamic> response;
   List arrayOfPurposes;
-  String date, time, duration;
+  String date, time, duration, selectedPurpose, repId;
+  var user;
+  SharedPreferences _preferences;
 
   @override
-  SubmitScreenState get initialState => DefaultState(
-        purposes: [
-          "purpose 1",
-          "purpose 2",
-          "purpose 3",
-        ],
-      );
+  SubmitScreenState get initialState => InitialSubmitScreenState();
 
   @override
   Stream<SubmitScreenState> mapEventToState(SubmitScreenEvent event) async* {
     if (event is SubmissionEvent) {
       try {
-        await _postSchedule(note: event.note);
+        await _postSchedule(
+          date: date,
+          time: time,
+          duration: duration,
+          purposeId: selectedPurpose,
+          repId: repId,
+          note: event.note,
+          userId: user["id"].toString(),
+        );
+        yield SuccessfulSubmissionState();
       } catch (e) {
         print("THE FOLLOWING ERROR HAS BEEN THROWN: $e");
-      } finally {
-        yield SuccessfulSubmissionState();
       }
+    } else if (event is FetchPreferencesDataEvent) {
+      await _initStateFromPrefs();
     } else if (event is PurposesFetchEvent) {
       try {
         await _fetchPurposes();
         _initArrayOfPurposes();
         yield DefaultState(
           purposes: arrayOfPurposes,
+          date: date,
+          time: time,
+          duration: duration,
         );
       } catch (e) {
         print("PURPOSES FETCH ENDED WITH AN ERROR: $e");
       }
     } else if (event is PurposeSelectionEvent) {
+      selectedPurpose = event.selectedPurpose;
       yield DefaultState(
         purposes: arrayOfPurposes,
-        selectedPurpose: event.selectedPurpose,
+        selectedPurpose: selectedPurpose,
+        date: date,
+        time: time,
+        duration: duration,
       );
     }
   }
@@ -58,12 +72,40 @@ class SubmitBloc extends Bloc<SubmitScreenEvent, SubmitScreenState> {
     arrayOfPurposes = (arrayOfPurposes.map((e) => e["purpose"])).toList();
   }
 
-  _postSchedule({String note}) async {
+  _postSchedule({
+    @required String date,
+    @required String time,
+    @required String duration,
+    @required String purposeId,
+    @required String repId,
+    @required String note,
+    @required String userId,
+  }) async {
     await _apiClient.postForm(
       SCHEDULE_POST_PATH,
       {
         "note": note,
+        "date": date,
+        "time": time,
+        "duration": duration,
+        "purposeId": purposeId,
+        "repId": repId,
+        "userId": userId,
       },
     );
+  }
+
+  Future<void> _initStateFromPrefs() async {
+    print("YOUR PREFERENCES ARE CURRENTLY UNFETCHED");
+    _preferences = await SharedPreferences.getInstance();
+    print("THE KEYS IN YOUR PREFERENCES ARE: ${_preferences.getKeys()}");
+    date = _preferences.getString("date");
+    time = _preferences.getString("time");
+    duration = _preferences.getString("duration");
+    repId = _preferences.getString("repId");
+    user = _preferences.get("user");
+    print(
+        "THE SHARED PREFERENCES YOU FETCHED ARE: \n DATE: $date \n TIME: $time \n DURATION: $duration");
+    print("THE USER THAT YOU SAVED TO PREFERENCES IS: ${user["id"]}");
   }
 }
