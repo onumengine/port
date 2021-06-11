ba6d78ff7ba61a844f460c0ac7743315d54fc269
onumdev <onumdev@gmail.com>
2021-06-11 08:51:28 +0100

    create an error component

---
+++ lib/components/molecules/error.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+
+class ErrorComponent extends StatelessWidget {
+  final String errorMessage;
+
+  ErrorComponent({@required this.errorMessage});
+
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          this.errorMessage,
+          textAlign: TextAlign.center,
+        ),
+        Text(
+          "You will be redirected to the previous page",
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
