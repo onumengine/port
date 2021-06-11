258b111928b1239e58eba818766dce95f4b19730
onumdev <onumdev@gmail.com>
2021-06-11 11:25:38 +0100

    wrap the content of the ErrorComponent in a Scaffold

--- lib/components/molecules/error.dart
+++ lib/components/molecules/error.dart
@@ -6,20 +6,22 @@ class ErrorComponent extends StatelessWidget {
   ErrorComponent({@required this.errorMessage});
 
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text(
-          this.errorMessage,
-          textAlign: TextAlign.center,
-        ),
-        Text(
-          "You will be redirected to the previous page",
-          textAlign: TextAlign.center,
-        ),
-      ],
+    return Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            this.errorMessage,
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            "You will be redirected to the previous page",
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
