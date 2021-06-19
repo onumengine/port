28d95e4339474a02bad89ada9a842bab44df59f1
onumdev <onumdev@gmail.com>
2021-06-19 17:05:54 +0100

    integrate the empty state component in the schedules screen

--- lib/screens/schedules.dart
+++ lib/screens/schedules.dart
@@ -7,11 +7,9 @@ import 'package:port/bloc/home/event.dart';
 import 'package:port/bloc/home/state.dart';
 import 'package:port/components/molecules/appointment_card.dart';
 import 'package:port/components/molecules/network_error.dart';
-import 'package:port/components/organisms/appointments.dart';
 import 'package:port/components/organisms/empty_appointments.dart';
 import 'package:port/screens/details.dart';
 import 'package:port/utility/colors.dart';
-import 'package:port/utility/colors_main.dart';
 
 class SchedulesScreen extends StatefulWidget {
   _SchedulesScreenState createState() => _SchedulesScreenState();
@@ -82,76 +80,90 @@ class _SchedulesScreenState extends State<SchedulesScreen> {
           child: BlocBuilder<HomeBloc, AppointmentState>(
               builder: (context, state) {
             if (state is BookedState) {
-              List approvedAppointments = state.otherSchedules.where((element) => element["action"] == "Approved").toList();
+              List approvedAppointments = state.otherSchedules
+                  .where((element) => element["action"] == "Approved")
+                  .toList();
               print("YOUR APPROVED APPOINTMENTS LIST IS $approvedAppointments");
 
               return TabBarView(
                 children: [
                   Center(
-                    child: (state.todaysSchedules.length == 0) ? EmptyAppointmentsComponent(
-                      instructionalText: "You don't have any appointments for today",
-                    ) : ListView.separated(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: _getHorizontalPadding(screenSize.width),
-                        vertical: 24,
-                      ),
-                      itemBuilder: (context, index) {
-                        return AppointmentCard(
-                          organizationName:
-                              state.todaysSchedules.elementAt(index)["org_name"],
-                          startTime:
-                              state.todaysSchedules.elementAt(index)["time"],
-                          approvalStatus: state.todaysSchedules
-                              .elementAt(index)["status"]
-                              .toString(),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => DetailsScreen(
-                                  appointment:
-                                      state.todaysSchedules.elementAt(index),
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                      separatorBuilder: (context, _) => SizedBox(height: 10),
-                      itemCount: state.todaysSchedules.length,
-                    ),
+                    child: (state.todaysSchedules.length == 0)
+                        ? EmptyAppointmentsComponent(
+                            instructionalText:
+                                "You don't have any appointments for today",
+                          )
+                        : ListView.separated(
+                            padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  _getHorizontalPadding(screenSize.width),
+                              vertical: 24,
+                            ),
+                            itemBuilder: (context, index) {
+                              return AppointmentCard(
+                                organizationName: state.todaysSchedules
+                                    .elementAt(index)["org_name"],
+                                startTime: state.todaysSchedules
+                                    .elementAt(index)["time"],
+                                approvalStatus: state.todaysSchedules
+                                    .elementAt(index)["status"]
+                                    .toString(),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => DetailsScreen(
+                                        appointment: state.todaysSchedules
+                                            .elementAt(index),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                            separatorBuilder: (context, _) =>
+                                SizedBox(height: 10),
+                            itemCount: state.todaysSchedules.length,
+                          ),
                   ),
                   Center(
-                    child: (approvedAppointments.length == 0) ? EmptyAppointmentsComponent(
-                      instructionalText: "You dont have any appointments for other days",
-                    ) : ListView.separated(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: _getHorizontalPadding(screenSize.width),
-                        vertical: 24,
-                      ),
-                      itemBuilder: (context, index) {
-                        return AppointmentCard(
-                          organizationName:
-                          approvedAppointments.elementAt(index)["org_name"],
-                          startTime:
-                          approvedAppointments.elementAt(index)["time"],
-                          approvalStatus: approvedAppointments.elementAt(index)["status"]
-                              .toString(),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => DetailsScreen(
-                                  appointment: approvedAppointments.elementAt(index),
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                      separatorBuilder: (context, _) => SizedBox(height: 10),
-                      itemCount: approvedAppointments.length,
-                    ),
+                    child: (approvedAppointments.length == 0)
+                        ? EmptyAppointmentsComponent(
+                            instructionalText:
+                                "You dont have any appointments for other days",
+                          )
+                        : ListView.separated(
+                            padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  _getHorizontalPadding(screenSize.width),
+                              vertical: 24,
+                            ),
+                            itemBuilder: (context, index) {
+                              return AppointmentCard(
+                                organizationName: approvedAppointments
+                                    .elementAt(index)["org_name"],
+                                startTime: approvedAppointments
+                                    .elementAt(index)["time"],
+                                approvalStatus: approvedAppointments
+                                    .elementAt(index)["status"]
+                                    .toString(),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => DetailsScreen(
+                                        appointment: approvedAppointments
+                                            .elementAt(index),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                            separatorBuilder: (context, _) =>
+                                SizedBox(height: 10),
+                            itemCount: approvedAppointments.length,
+                          ),
                   ),
                 ],
               );
