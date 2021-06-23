81f09652613747fa642beb804f19660c66be6ac0
onumdev <onumdev@gmail.com>
2021-06-23 17:41:32 +0100

    add a var that determines if SubmitScreen will open

--- lib/bloc/scheduler/state.dart
+++ lib/bloc/scheduler/state.dart
@@ -3,8 +3,8 @@ import 'package:equatable/equatable.dart';
 abstract class SchedulerScreenState extends Equatable {}
 
 class ScheduleState extends SchedulerScreenState {
-  int selectedTimeButtonIndex, selectedDurationButtonIndex;
-  Map<int, String> availableTimes, availableDurations;
+  final int selectedTimeButtonIndex, selectedDurationButtonIndex;
+  final Map<int, String> availableTimes, availableDurations;
 
   ScheduleState({
     this.selectedTimeButtonIndex,
@@ -19,5 +19,28 @@ class ScheduleState extends SchedulerScreenState {
         this.selectedDurationButtonIndex,
         this.availableTimes,
         this.availableDurations,
+      ];
+}
+
+class ScheduleClearForSubmissionState extends ScheduleState {
+  final int selectedTimeButtonIndex, selectedDurationButtonIndex;
+  final Map<int, String> availableTimes, availableDurations;
+  final bool proceedToSubmission;
+
+  ScheduleClearForSubmissionState({
+    this.selectedTimeButtonIndex,
+    this.selectedDurationButtonIndex,
+    this.availableTimes,
+    this.availableDurations,
+    this.proceedToSubmission,
+  });
+
+  @override
+  List<Object> get props => [
+        this.selectedTimeButtonIndex,
+        this.selectedDurationButtonIndex,
+        this.availableTimes,
+        this.availableDurations,
+        this.proceedToSubmission,
       ];
 }
